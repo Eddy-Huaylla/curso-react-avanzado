c7cca7a8b6cbbd2e36434df7003d5778ca0c8622
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-12-14 17:06:57 -0500

    Uso de polyfill de Intersection Observer e imports dinámicos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,23 +4,30 @@ import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE } ) => {
 	const element          = useRef( null )
 	const[ show, setShow ] = useState( false )
 
 
 	useEffect( () => {
-		const observer = new window.IntersectionObserver( ( entries ) => {
-			const { isIntersecting } = entries[0]
-			console.log( isIntersecting )
+		Promise.resolve(
+			typeof window.IntersectionObserver !== 'undefined'
+			? window.IntersectionObserver
+			: import('intersection-observer')
+		)
+		.then( () => {
+			const observer = new window.IntersectionObserver( ( entries ) => {
+				const { isIntersecting } = entries[0]
 
-			if( isIntersecting ) {
-				setShow (true )
-				observer.disconnect()
-			}
+				if( isIntersecting ) {
+					setShow(true )
+					observer.disconnect()
+				}
+			})
+
+			observer.observe( element.current )
 		})
 
-		observer.observe( element.current )
 	}, [ element ] )
 
 
@@ -35,7 +42,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 					</a>
 
 					<Button>
-						<MdFavoriteBorder size = '32px' /> {likes} likes!
+						<MdFavoriteBorder size = '32px' /> { likes } likes!
 					</Button>
 				</Fragment>
 			}
