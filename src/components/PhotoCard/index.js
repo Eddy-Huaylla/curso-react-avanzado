a225be8fe3caa2f4515be7f4a0902abf2af3c5be
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-12-14 17:18:44 -0500

    Usando el localStorage para guardar los likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -8,12 +8,35 @@ export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE } ) => {
 	const element          = useRef( null )
 	const[ show, setShow ] = useState( false )
 
+	const key = `like-${id}`
+
+	const [ liked, setLiked ] = useState( () => {
+		try {
+			const like = window.localStorage.getItem(key)
+			return like
+		} catch (e) {
+			return false
+		}
+	})
+
+	const setLocalStorage = value => {
+		try {
+			window.localStorage.setItem( key, value )
+			setLiked( value )
+		} catch (e) {
+			console.error( e )
+		}
+	}
+
+
+	const Icon = liked ? MdFavorite : MdFavoriteBorder
+
 
 	useEffect( () => {
 		Promise.resolve(
 			typeof window.IntersectionObserver !== 'undefined'
 			? window.IntersectionObserver
-			: import('intersection-observer')
+			 :  import('intersection-observer')
 		)
 		.then( () => {
 			const observer = new window.IntersectionObserver( ( entries ) => {
@@ -41,8 +64,8 @@ export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE } ) => {
 						</ImgWrapper>
 					</a>
 
-					<Button>
-						<MdFavoriteBorder size = '32px' /> { likes } likes!
+					<Button onClick = { () => setLocalStorage( !liked) } >
+						<Icon size = '32px' /> { likes } likes!
 					</Button>
 				</Fragment>
 			}
