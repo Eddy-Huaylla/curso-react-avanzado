4f6d0bad5303cb1802f34b204c707bc245134241
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-12-14 12:45:15 -0500

    useEffect: limpiando eventos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
 	const [ categories, setCategories ] = useState( [] );
+	const [ showFixed, setShowFixed ] = useState( false )
 
 	useEffect( () => {
 		window.fetch('https://petgram-server-jhe-eddy-huaylla.vercel.app/categories')
@@ -14,13 +15,29 @@ export const ListOfCategories = () => {
 		})
 	}, [] )
 
-	return (
-		<List>
-		{
-			categories.map(
-				category => <Item key={ category.id }><Category { ...category } /></Item>
-			)
+	useEffect( () => {
+		const onScroll = e => {
+			const newShowFixed = window.scrollY > 200
+			showFixed !== newShowFixed && setShowFixed(newShowFixed)
 		}
+
+		document.addEventListener('scroll', onScroll)
+
+		return () => document.removeEventListener('scroll', onScroll)
+	}, [ showFixed ])
+
+	const renderList = (fixed) => (
+		<List className={fixed ? 'fixed' : ''}>
+			{
+				categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+			}
 		</List>
+	)
+
+	return (
+		<Fragment>
+			{ renderList() }
+			{ showFixed && renderList(true) }
+		</Fragment>
 	)
 }
