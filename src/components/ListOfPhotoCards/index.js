57fb838efa87b0d0d29d86019d65d122efac685e
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-12-14 21:13:55 -0500

    Parámetros para un query con GraphQL

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useQuery } from '@apollo/client';
 import { PhotoCard } from '../PhotoCard'
-import listPhotos from '../../graphql/photo/queries/list-photos.gql'
+import LIST_PHOTOS from '../../graphql/photo/queries/list-photos.gql'
+import LIST_PHOTOS_BY_CATEGORY from '../../graphql/photo/queries/list-photos-by-category.gql'
 
-export const ListOfPhotoCards = () => {
-	const { loading, error, data } = useQuery( listPhotos );
+export const ListOfPhotoCards = ( { categoryId = 0 } ) => {
+	const { loading, error, data } = categoryId > 0 ? useQuery( LIST_PHOTOS_BY_CATEGORY, { variables : {
+		categoryId : categoryId
+	}} ) : useQuery ( LIST_PHOTOS )
 
 	if ( error ) {
 		return <h2>Internal Server Error</h2>
