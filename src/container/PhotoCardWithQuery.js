ba04001e883c12e7f48bfe522325274d75416688
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-12-14 21:49:30 -0500

    Refactorizando y usando variables de loading y error

--- src/container/PhotoCardWithQuery.js
+++ src/container/PhotoCardWithQuery.js
@@ -3,16 +3,16 @@ import { PhotoCard } from '../components/PhotoCard'
 import { Query } from '@apollo/client/react/components';
 import GET_PHOTO from '../graphql/photo/queries/get-photo.gql'
 
+const render = ( { loading, error, data } ) => {
+	const { photo } = data || { photo: {} };
+
+	if( error ) return <h2> Internal Server Error </h2>
+
+	return loading ? <PhotoCard /> : <PhotoCard {...photo} />
+}
+
 export const PhotoCardWithQuery = ( { id } ) => (
 	<Query query = { GET_PHOTO } variables = { { id } } >
-		{
-			( { loading, error, data } ) => {
-				const { photo } = data || { photo: {} };
-
-				if( error ) return <h2> Internal Server Error </h2>
-
-				return loading ? <PhotoCard /> : <PhotoCard {...photo} />
-			}
-		}
+		{ render }
 	</Query>
 )
